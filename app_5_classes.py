33bc1c9aec536af0117b8bac9df6018eb0d72d42
super cow <bullandrooster@gmail.com>
2020-04-19 13:50:54 +0200

    classes ostatak

--- app_5_classes.py
+++ app_5_classes.py
@@ -1,3 +1,6 @@
+from abc import ABC, abstractmethod
+from collections import namedtuple
+
 class Point:  # PascalNamingConvetion
     default_color = "red"  # class attribute
 
@@ -100,6 +103,8 @@ print(cloud._TagCloud__tags)  # {'python': 100}
 # properties, get set
 
 # ovako bi napisali u c# ili javi
+
+
 class Product1:
     def __init__(self, price):
         # self.__price = price
@@ -107,16 +112,19 @@ class Product1:
 
     def get_price(self):
         return self.__price
-    
+
     def set_price(self, value):
         if value < 0:
             raise ValueError("Price cannot be negative.")
         else:
             self.__price = value
 
+
 print(Product1(50).get_price())
 
 # ovako mozes pisati u pythonu, ali ipak nemoj
+
+
 class Product2:
     def __init__(self, price):
         # self.__price = price
@@ -124,7 +132,7 @@ class Product2:
 
     def __get_price(self):
         return self.__price
-    
+
     def __set_price(self, value):
         if value < 0:
             raise ValueError("Price cannot be negative.")
@@ -132,9 +140,12 @@ class Product2:
             self.__price = value
     price = property(__get_price, __set_price)
 
+
 print(Product2(51).price)
 
 # ovako pisi u pythonu
+
+
 class Product3:
     def __init__(self, price):
         self.price = price
@@ -150,4 +161,147 @@ class Product3:
         else:
             self.__price = value
 
+
 print(Product3(52).price)
+
+# inheritance, overriding
+
+
+class Animal:
+    def __init__(self):
+        self.age = 1
+
+    def eat(self):
+        print("eat")
+
+
+class Mammal(Animal):
+    def __init__(self):  # override Animal constructora
+        super().__init__()  # da ovo nisi napisao ne bi se izvrsila base constructor metoda Animala
+        self.weight = 2
+
+    def walk(self):
+        print("walk")
+
+
+class Chicken(Animal):  # empty class
+    pass
+
+
+m = Mammal()
+m.eat()
+#m.age = 2
+print(m.age)
+print(isinstance(m, Animal))  # True
+print(isinstance(m, Mammal))  # True
+print(isinstance(m, object))  # True
+obj = object()  # base object class
+print(issubclass(Mammal, Animal))  # True
+
+# multiple inheritance
+# avoid multi level inheritace!! Employee - Persosn - LivingCreature -Thing, max 1 - 2 levels
+
+# bad example, same methods
+
+
+class Employee:
+    def greet(self):
+        print("Employee greet")
+
+
+class Person:
+    def greet(self):
+        print("Person greet")
+
+
+class Manager(Employee, Person):
+    pass
+
+
+manager = Manager()
+manager.greet()  # Employerr greet, a ne Person greet
+
+# good example of inheritance
+
+
+# by convetnion, all custom Errors must end with Error sufix
+class InvalidOperationError(Exception):
+    pass
+
+# ABC - abstract base class!
+class Stream(ABC): # abstract, ne mozes instancirati klasu
+    def __init__(self):
+        self.opened = False
+
+    def open(self):
+        if self.opened:
+            raise InvalidOperationError("Stream is already opened.")
+        else:
+            self.opened = True
+
+    def close(self):
+        if not self.opened:
+            raise InvalidOperationError("Stream is already closed.")
+        else:
+            self.opened = False
+    
+    @abstractmethod
+    def read(self):
+        pass
+
+class FileStream(Stream):
+    def read(self):
+        print("Reading data from a file.")
+
+class NetworkStream(Stream):
+    def read(self):
+        print("Reading data from a network.")
+
+# Polymorphism
+# The word polymorphism means having many forms. ... 
+# Real life example of polymorphism: A person at the same time can have different characteristic. 
+# Like a man at the same time is a father, a husband, an employee.
+# So the same person posses different behaviour in different situations. This is called polymorphism.
+
+# Polymorphism, duck typing
+# npr neka metoda prima listu razlicitih objekata koji imaju implementiranu istu metodu, i onda se ona izvrsava.
+
+# extending builtin types
+class Text(str): # napravis duplicate metodu nad string objektom
+    def duplicate(self):
+        return self + self
+
+text = Text("Python")
+print(text.lower())
+print(text.duplicate()) #nova metoda
+
+class TrackableList(list):
+    def append(self, object):
+        print("Append called.")
+        super().append(object)
+
+list = TrackableList()
+list.append(1)
+
+# data classes, class with no methods
+
+class MyPoint:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+# da ne moras ovo raditi, postoji namedtuple.. gledaj ispod
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+p1 = MyPoint(1, 2)
+p2 = MyPoint(1, 2)
+print(p1 == p2) # False, True zbog custom __eg__ magic metode
+print(id(p1)) # 14899760 address in memory
+print(id(p2)) # 14899808 address in memory
+
+# custom namedtuple type, PascalCase
+MyTuplePoint = namedtuple("Point", ["x", "y"])
+p1 = MyTuplePoint(x=1,y=2)
+p2 = MyTuplePoint(y=2,x=1)
+print(p1 == p2) # True
