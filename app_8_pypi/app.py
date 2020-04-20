077bba67ef550c5aa8ce501d7500309edc122eae
super cow <bullandrooster@gmail.com>
2020-04-20 14:50:06 +0200

    pipenv

--- app_8_pypi/app.py
+++ app_8_pypi/app.py
@@ -31,3 +31,37 @@ print(request.status_code) # 200
 # pipenv --venv --> path gdje se nalazi virtualni direktorij, nije vise u projektu, sto je dobro!
 # pipenv shell --> aktiviras virtualni cmd i iz nje mozes pokrenuti python program: python app.py, tako da kuzi gdje su dependeciji
 # exit --> izadjes env cmd-a
+
+# vs code, py runner, kazes koji python da koristi
+# pipenv --venv -> env folder
+# onda u vs code settings.json, nadjes code-runner.executorMap i nadjes "python".. stavis putanju od env pythona (na linuxu je naziv fajla python3 na win samo python)
+
+# py runner postavke.. kad ti bude trebalo, pogledaj opet video.
+
+# pipenv
+# PipFile, popis papketa i struktura, slicno kao npm package.js
+# PipFile.lock slicno kao npm package-lock.lock # popis tocnih verzija paketa
+
+# pipenv install, slicno kao npm install
+# pipenv install --ignore-pipfile --> instalira tocne verzije paketa koje su definrane u Pipfile.lock fajlu
+
+# managing dependencies
+# pipenv graph --> izlista sve dependecije i njihove dependencije
+# pipenv uninstall packagename, makne packagename ali ne makne njegove dependecije jer ne zna da li ih neko koristi
+# pipenv graph --> prikaze dependecije koje nije maknio, ako se projekt instalira negde drugde, tih dependecija vise nece biti
+
+# update paketa
+# pipenv update --outdated
+
+# update pojedinog paketa
+# pipenv update packagename
+
+
+# publishig package
+# skip
+
+# document published package, docstring
+# skip
+
+# pydoc utility, see documentation of module
+# pydoc math
